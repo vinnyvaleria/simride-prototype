a78a91d8041a1d2f03baf54fb7a9acbff9fcb637
Shafiq <58117391+xshafqx@users.noreply.github.com>
2020-05-07 17:01:03 +0800

    update get directions from selecting user

--- simride/components/maps/map.jsx
+++ simride/components/maps/map.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable promise/catch-or-return */
+/* eslint-disable no-alert */
+import firebase from '../../base';
 import React from 'react';
 import { Text, View, Button, StyleSheet } from 'react-native';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
@@ -5,18 +8,24 @@ import Geocode from "react-geocode";
 Geocode.enableDebug();
 import getDirections from 'react-native-google-maps-directions'
 
+let postal = []
+let ppl = []
+let towards
+
 class map extends React.Component {
 
     constructor() {
         super();
         this.state = {
             ready: false,
-            where: { lat: null, lng: null },
+            to: { lat: null, lng: null },
+            from: { lat: null, lng: null },
             error: null
         }
     }
 
     componentDidMount() {
+        this.getPostal('bookingID');
         let geoOptions = {
             enableHighAccuracy: true,
             timeOut: 20000,
@@ -32,7 +41,7 @@ class map extends React.Component {
         console.log(position.coords.longitude);
         this.setState({
             ready: true,
-            where: { lat: position.coords.latitude, lng: position.coords.longitude }
+            from: { lat: position.coords.latitude, lng: position.coords.longitude }
         })
     }
 
@@ -40,16 +49,73 @@ class map extends React.Component {
         this.setState({ error: err.message });
     }
 
+    getPostal = () => {
+        const database = firebase.database().ref('bookings');
+        database.once('value', (snapshot) => {
+            if (snapshot.exists()) {
+                snapshot.forEach((data) => {
+                    if (data.key === '-M5lzRNnCCt5RwXp9zmb') { // replace with bookingID later
+                        towards = data.val().towards;
+
+                        if (data.val().currPassengers !== "") {
+                            ppl = data.val().currPassengers.split(', ');
+                            postal = data.val().postal.split(', ');
+                        }
+                    }
+                });
+
+                for (var d = 0; d < ppl.length; d++) {
+                    var btn = document.createElement('input');
+                    btn.setAttribute('type', 'button')
+                    btn.setAttribute('value', ppl[d]);
+                    btn.setAttribute('id', postal[d]);
+                    btn.onclick = this.plotPts;
+                    document.getElementById('directions').appendChild(btn);
+                    console.log(btn)
+                }
+            }
+        });
+    }
+
+    plotPts = (e) => {
+        postal = e.target.id;
+        let latlng = postal.split(':');
+        if (towards === 'Home') {
+            this.setState({
+                to: {
+                    lat: parseFloat(latlng[0]),
+                    lng: parseFloat(latlng[1])
+                },
+                from: {
+                    lat: 1.329426,
+                    lng: 103.776571
+                },
+            })
+        }
+        else {
+            this.setState({
+                from: {
+                    lat: parseFloat(latlng[0]),
+                    lng: parseFloat(latlng[1])
+                },
+                to: {
+                    lat: 1.329426,
+                    lng: 103.776571
+                },
+            })
+        }
+    }
+
     //Testing1 This shows route, but it brings you to the actual google maps
     handleGetDirections = () => {
         const data = {
             source: {
-                latitude: 1.329426,
-                longitude: 103.776571
+                latitude: this.state.from.lat,
+                longitude: this.state.from.lng
             },
             destination: {
-                latitude: this.state.where.lat,
-                longitude: this.state.where.lng
+                latitude: this.state.to.lat,
+                longitude: this.state.to.lng
             },
             params: [
                 {
@@ -63,7 +129,7 @@ class map extends React.Component {
             ]
         }
         getDirections(data)
-    }//End of Testing1
+    }
 
     render() {
         return (
@@ -75,23 +141,25 @@ class map extends React.Component {
                     <Text>{this.state.error}</Text>
                 )}
                 {this.state.ready && (
-                    <Text>Latitude:{this.state.where.lat}, Longitude:{this.state.where.lng}
+                    <Text>Latitude:{this.state.to.lat}, Longitude:{this.state.to.lng}
                         <div>
+                            <div id='directions'></div>
                             <h1 align="center">WELCOME TO GOOGLE MAPS</h1>
                         </div>
                         <div>
                             <Button onPress={this.handleGetDirections} title="Get Directions" />
-                            <Map google={this.props.google} zoom={16} initialCenter={{ lat: this.state.where.lat, lng: this.state.where.lng }}>
+
+                            <Map google={this.props.google} zoom={16} initialCenter={{ lat: this.state.from.lat, lng: this.state.from.lng }}>
                                 <Marker onClick={this.onMarkerClick}
                                     name={'Current location'} />
                                 <Marker
-                                    name={'Singapore Institute of Management'}
-                                    position={{ lat: 1.329426, lng: 103.776571 }} />
-                                
+                                    name={'Destination'}
+                                    position={{ lat: this.state.to.lat, lng: this.state.to.lat }} />
+
                             </Map>
                         </div>
                     </Text>
-                )}             
+                )}
             </View>
         );
     }
