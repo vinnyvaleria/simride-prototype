0925b6ade99a8a81794fae1c78c3071a1accb807
utyumba <vvi001@mymail.sim.edu.sg>
2020-05-29 23:25:06 +0800

    update changes with inbox, left redirection

--- simride/components/inbox/index.jsx
+++ simride/components/inbox/index.jsx
@@ -269,7 +269,6 @@ class Inbox extends React.Component {
     inboxMsgButton() {
         if (document.getElementById("chatsStarted") !== null) {
             document.getElementById("chatsStarted").innerHTML = "";
-            document.getElementById('searchUser').style.display = "none";
             document.getElementById('inbox').style.display = "block";
             document.getElementById('sendNewMessage').style.display = "none";
             document.getElementById('msgBox').style.display = "none";
@@ -296,6 +295,7 @@ class Inbox extends React.Component {
     openChat(e) {
         document.getElementById('msgBox').style.display = "block";
         document.getElementById('chatsStarted').style.display = "none";
+        document.getElementById('searchUser').style.display = "none";
         document.getElementById("messages").innerHTML = "";
 
         chatName = chats[e.target.id];
@@ -495,18 +495,20 @@ class Inbox extends React.Component {
           <View style={{ width: '100%', justifyContent: "center", alignItems: "center" }}>
                 <div id='msgsPage'>
                     <div>
-                        <div>
-                            <h1>Messages</h1>
+                        <div id='searchUser'>
+                            <h1>Inbox</h1>
+                            <input id="selectUser" placeholder="Search user" value={this.state.to} onChange={this.handleChange}
+                                type="text" name="to" style={{ width: '350px' }} />
+                            <button id='submitSearchUserButton' onClick={this.searchUsername}>Submit</button>
                         </div>
-                        <div id='msgOption'>
+                        <div id='msgOption' style={{ display: 'none' }}>
                             <button id='inboxMsgButton' title="Inbox" onClick={this.inboxMsgButton}>Inbox</button>
                             <button id='newMsgButton' title="newMessage" onClick={this.newMsgButton}>Search User</button>
                         </div>
-                        <br />
                         <div id='inbox'>
                             <div id='chatsStarted'></div>
                             <div id='msgBox' style={{ display: 'none' }}>
-                                <h2>{this.state.to}</h2>
+                                <h1>{this.state.to}</h1>
                                 <div id="messages"></div>
                                 <div id="submitInboxMessage" style={{ display: 'none' }}>
                                   <input id="message" placeholder="Enter message" value={this.state.message}
@@ -514,16 +516,7 @@ class Inbox extends React.Component {
                                   <button style={{ margin: '0 0 0 15px' }} id='submitMsgButton' onClick={this.sendMessage}>Submit</button>
                                 </div>
                             </div>
-                            
                         </div>
-
-                        <br />
-                        <div id='searchUser' style={{ display: 'none' }}>
-                            <input id="selectUser" placeholder="Search user" value={this.state.to} onChange={this.handleChange}
-                                type="text" name="to" style={{ width: '350px' }} />
-                            <button id='submitSearchUserButton' onClick={this.searchUsername}>Submit</button>
-                        </div>
-
                         <div id="sendNewMessage" style={{ display: 'none' }}>
                             <button id='chattingTo' onClick={this.viewUserProfile}></button>
                             <div>
